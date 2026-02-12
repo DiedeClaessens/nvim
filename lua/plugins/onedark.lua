6aaa75c7581954d6a34b91c9a48e6629d86fe33e
Diede Claessens <diede@starfish.team>
2026-02-12 18:42:59 +0100

    darker bg color

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -1,9 +1,15 @@
 return {
-    "navarasu/onedark.nvim",
-		config = function()
-      require('onedark').setup {
-        style = 'darker'
+  "navarasu/onedark.nvim",
+  config = function()
+    require('onedark').setup {
+      style = 'darker',
+      colors = {
+        bg0 = "#0a0c10",   -- Much darker background
+        bg_d = "#05070a",  -- Even darker variant
+        bg1 = "#0f1115",   -- Darker bg1 to match
+        black = "#000000", -- Pure black
       }
-      require('onedark').load()
-    end
+    }
+    require('onedark').load()
+  end
 }
